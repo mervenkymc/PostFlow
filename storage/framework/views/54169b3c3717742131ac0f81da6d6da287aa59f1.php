ec6307111231345cda028d8ca0026379ebc033b0
Mervenur Kuyumcu <mervenurkuyumcu@gmail.com>
2020-05-01 14:07:49 +0300

    Setup

---
+++ storage/framework/views/54169b3c3717742131ac0f81da6d6da287aa59f1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PostFlow</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\postflow\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f701c644d6afb3bd0af109c0e678ef0dc68fb9e3.php
@@ -0,0 +1,4 @@
+<?php
+//after sign up
+    echo "merve";
+ ?><?php /**PATH C:\xampp\htdocs\postflow\resources\views/mainpage.blade.php ENDPATH**/ ?>
